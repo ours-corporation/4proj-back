5b78d80c6121686d2b32026d06603c14a90b601b
mathislecomte <lecomtemathis31@gmail.com>
2026-02-24 10:58:42 +0100

    swagger : edit prod domain

--- src/swagger.ts
+++ src/swagger.ts
@@ -16,7 +16,7 @@ const options: swaggerJsdoc.Options = {
                 description: 'Serveur de développement local',
             },
             {
-                url: 'https://api.supfile.supchat.org/api',
+                url: 'https://api.supfile.dev-lecomte.fr/api',
                 description: 'Serveur de production',
             }
         ],
