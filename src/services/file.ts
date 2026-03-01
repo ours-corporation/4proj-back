e11b0ae2494e95bcfdda9f8386c4f7522c231e40
Goldlulu <luc.mialhe@hotmail.com>
2026-03-01 13:27:32 +0100

    chore: remove debug logs, dead code and trivial comments pre-merge

--- src/services/file.ts
+++ src/services/file.ts
@@ -137,12 +137,8 @@ class FileService {
         const sourcePath = path.join(UPLOAD_ROOT, file.user_id.toString(), file.physical_key);
         const newPhysicalKey = uuidv4();
         const userDir = path.join(UPLOAD_ROOT, userId.toString());
-
-        if (!fs.existsSync(userDir)) {
-            fs.mkdirSync(userDir, { recursive: true });
-        }
-
         const targetPath = path.join(userDir, newPhysicalKey);
+
         await fs.promises.mkdir(userDir, { recursive: true });
         await fs.promises.copyFile(sourcePath, targetPath);
 
